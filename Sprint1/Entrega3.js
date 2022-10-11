d5b189e94d906342c3c5a74ed0337e3ba3cdfb89
oriolsastre <oriol.sastre@gmail.com>
2022-10-11 12:52:36 +0200

    entrega3

--- Sprint1/Entrega3.js
+++ Sprint1/Entrega3.js
@@ -3,3 +3,18 @@
 /* Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep.
 Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent de si la Promise es resol o no. */
 
+//let esMult3 = i => 
+
+for(let i=1;i<11;i++){
+    const calcMult3 = new Promise((resolve,reject) => {
+        if(i%3===0){
+            resolve(`${i} és múltiple de 3.`);
+        }else{
+            reject(`${i} NO és múltiple de 3.`)
+        }
+    })
+
+    calcMult3
+        .then( res => {console.log(res)})
+        .catch( err => {console.log(err)})
+}
