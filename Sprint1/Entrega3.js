c2a18fffd91faca4e1d9435dce1a6b6c04eafba0
oriolsastre <oriol.sastre@gmail.com>
2022-10-11 12:14:52 +0200

    crear entrega 3

---
+++ Sprint1/Entrega3.js
@@ -0,0 +1,5 @@
+/* Nivell 1 */
+// Exercici 1
+/* Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep.
+Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent de si la Promise es resol o no. */
+
