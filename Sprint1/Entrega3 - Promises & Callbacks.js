98a05e80b3dd3a14ee30582dfe0633bd1792b499
oriolsastre <oriol.sastre@gmail.com>
2022-10-11 12:58:15 +0200

    Entrega3.1

--- Sprint1/Entrega3 - Promises & Callbacks.js
+++ Sprint1/Entrega3 - Promises & Callbacks.js
@@ -4,26 +4,19 @@
 Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent de si la Promise es resol o no. */
 
 let esMult3 = i => {
-    const calcMult3 = new Promise((resolve,reject) => {
+    const calcMult3 = new Promise( (resolve,reject) => {
         if(i%3===0){
             resolve(`${i} és múltiple de 3.`);
         }else{
             reject(`${i} NO és múltiple de 3.`);
         }
-    }
+    })
     return calcMult3;
 }
 
-/* for(let i=1;i<11;i++){
-    const calcMult3 = new Promise((resolve,reject) => {
-        if(i%3===0){
-            resolve(`${i} és múltiple de 3.`);
-        }else{
-            reject(`${i} NO és múltiple de 3.`)
-        }
-    })
-
-    calcMult3
+for(let i=1;i<11;i++){
+    const comprova3 = esMult3(i);
+    comprova3
         .then( res => {console.log(res)})
         .catch( err => {console.log(err)})
-} */
+}
