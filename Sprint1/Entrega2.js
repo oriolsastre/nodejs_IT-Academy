1198d39738b281e70a4739feb806f68c6afc1c6d
oriolsastre <oriol.sastre@gmail.com>
2022-10-11 09:34:22 +0200

    Fent exercicis

--- Sprint1/Entrega2.js
+++ Sprint1/Entrega2.js
@@ -1,9 +1,31 @@
-// Nivell 1
+/* Nivell 1 */
 // Exercici 1
 // Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.
 
 console.log( ( (a,b) => a+b )(1,2) );
 
-// Nivell 2
+/* Nivell 2 */
 // Exercici 1
 // Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
+
+var moble = {};
+
+// Exercici 2
+/* Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'.
+Invoca el mètode dirNom des de fora de la classe. */
+
+class Persona {
+    constructor(nom){
+        this.nom=nom;
+    }
+    dirNom() {
+        console.log(this.nom);
+    }
+}
+
+var jo = new Persona("Oriol");
+jo.dirNom();
+
+/* Nivell 3 */
+// Exercici 1
+// Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta. Invoca-la amb diferents definicions.
