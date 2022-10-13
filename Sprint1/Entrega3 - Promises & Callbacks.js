4a7a112b5136ffda39a24c701c295f6deb1ae5db
oriolsastre <oriol.sastre@gmail.com>
2022-10-13 09:54:07 +0200

    Entrega 3 exercici 1

--- Sprint1/Entrega3 - Promises & Callbacks.js
+++ Sprint1/Entrega3 - Promises & Callbacks.js
@@ -16,7 +16,47 @@ let esMult3 = i => {
 
 for(let i=1;i<11;i++){
     const comprova3 = esMult3(i);
-    comprova3
+    /* comprova3
         .then( res => {console.log(res)})
-        .catch( err => {console.log(err)})
+        .catch( err => {console.log(err)}) */
+    comprova3.then(res => {console.log(res)}, err => {console.log(err)})
+}
+
+// Exercici 2
+// Crea una arrow function que rebi un paràmetre i una funció callback i li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre rebut.
+let mostraMissatge = (num,ite) => {console.log(`La suma iterativa dels digits de ${num} és ${ite}`)};
+const sumaIterativa = (nombre, callback) => {
+    //Exemple: 159 => 1+5+9=15 => 1+5=6
+    const iteratiu = nombre%9;
+    callback(nombre,iteratiu);
+}
+sumaIterativa(159,mostraMissatge);
+
+
+/* Nivell 2 */
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+// Donats els objectes employees i salaries, crea una arrow function getEmployee() que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+let getEmployee = (employees,id) => {
+
 }
