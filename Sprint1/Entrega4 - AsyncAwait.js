e624185f7af7a63ac3a9ccac0463dca16ab02b2f
oriolsastre <oriol.sastre@gmail.com>
2022-10-18 09:39:51 +0200

    entrega412

--- Sprint1/Entrega4 - AsyncAwait.js
+++ Sprint1/Entrega4 - AsyncAwait.js
@@ -1,3 +1,54 @@
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+let getEmployee = id => {
+    return new Promise ((resolve, reject) => {
+        if(typeof id !== 'number'){reject(new Error(`L'ID ha de ser un número.`))}
+        var trobat = false;
+        var empleat = null;
+        for(var index in employees){
+            if(employees[index].id === id){
+                trobat = true;
+                empleat = employees[index];
+            }
+        }
+        if(trobat){resolve(empleat);}
+        else{reject(new Error(`No s'ha trobat cap empleat amb id ${id}.`))}
+    })
+}
+let getSalary = employee => {
+    return new Promise((resolve,reject) => {
+        //comprovar que l'objecte employee té una id
+        if(typeof employee.id !== 'number'){reject(new Error(`Objecte d'empleat invàlid o sense id vàlida!`))}
+        
+        for(var index in salaries){
+            if(salaries[index].id == employee.id){
+                resolve(salaries[index].salary);
+            }
+        }
+        reject(new Error("No s'ha trobat aquest empleat."))
+    })
+    
+}
+
 /* Nivell 1 */
 // Exercici 1
 /* Crea una funció asíncrona que rebi un id d'empleat/da i imprimeixi per pantalla el nom de l'empleat/da i el seu salari,
@@ -11,8 +62,21 @@ async function empleatSalari(id){
         console.log(err.message);
     }
 }
-// Exercici 2
+//comprovació
+empleatSalari(2);
 
+// Exercici 2
+// Crea una nova funció asíncrona que cridi a una altra que retorni una Promise que efectuï la seva funció resolve() després de 2 segons de la seva invocació.
+async function cridaFuncio(){
+    try{
+        const empleat = await getEmployee(2);
+        setTimeout(()=>{
+            console.log(`Dos segons més tard et dic que aquest empleat es diu ${empleat.name}`);
+        },2000)
+    }catch(err){console.log(err.message);}
+}
+//comprovacio
+cridaFuncio();
 
 /* Nivell 2 */
 // Exercici 1
