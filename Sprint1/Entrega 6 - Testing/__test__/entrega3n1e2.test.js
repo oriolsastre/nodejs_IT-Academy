f8779669f0106b2feef8853aa856fa424853a0da
Oriol Sastre <oriol.sastre@gmail.com>
2022-10-24 19:43:57 +0200

    testing 2

--- Sprint1/Entrega 6 - Testing/__test__/entrega3n1e2.test.js
+++ Sprint1/Entrega 6 - Testing/__test__/entrega3n1e2.test.js
@@ -1,1 +1,13 @@
-const n1e1 = require('../app/entrega3n1e2')
+const n1e2 = require('../app/entrega3n1e2')
+
+test("Només accepta números", () => {
+    const mostraMissatge = n1e2.mostraMissatge
+    const callbackNull = (n,i) => {return n;}
+    expect(()=>{
+        n1e2.sumaIterativa("b",mostraMissatge).toThrow(Error)
+    })
+    expect(()=>{
+        n1e2.sumaIterativa(Infinity,mostraMissatge).toThrow(Error)
+    })
+    expect(n1e2.sumaIterativa(19,callbackNull)).toBe(1);
+})
