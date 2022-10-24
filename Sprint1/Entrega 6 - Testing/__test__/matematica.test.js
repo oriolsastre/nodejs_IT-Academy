5d3fb2a1c8a333b9bca0316fcb325c384d2c0056
Oriol Sastre <oriol.sastre@gmail.com>
2022-10-24 15:44:57 +0200

    test matematica

--- Sprint1/Entrega 6 - Testing/__test__/matematica.test.js
+++ Sprint1/Entrega 6 - Testing/__test__/matematica.test.js
@@ -1,6 +1,6 @@
 const matematica = require('../app/matematica')
 
-test("Sumar/Restar/Multiplicar/Dividir 3 elements", ()=>{
+test("Sumar/Restar/Multiplicar/Dividir 3 enters", ()=>{
     expect(matematica.sumar(1,2,3)).toBe(6);
     expect(matematica.restar(3,2,1)).toBe(0);
     expect(matematica.multiplicar(1,2,3)).toBe(6);
@@ -12,6 +12,13 @@ test("Fer operacions amb números negatius", () => {
     expect(matematica.restar(3,-2,1)).toBe(4);
     expect(matematica.multiplicar(1,2,-3)).toBe(-6);
     expect(matematica.dividir(8,-4,-2)).toBe(1);
+})
+
+test("Fer operacions amb decimals", () => {
+    expect(matematica.sumar(0.25,1.78,-2.56)).toBe(0.25+1.78-2.56);
+    expect(matematica.restar(0.25,1.78,-2.56)).toBe(0.25-1.78+2.56);
+    expect(matematica.multiplicar(0.25,1.78,-2.56)).toBe(0.25*1.78*-2.56);
+    expect(matematica.dividir(0.25,1.78,-2.56)).toBe((0.25/1.78)/-2.56);
 })
 
 test("No es pot dividir per 0", () => {
